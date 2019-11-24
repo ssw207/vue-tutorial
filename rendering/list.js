27e7165a69de746b2f3c58a1e3384688de51b2cd
swsong89 <ssw890720@gmail.com>
2019-11-24 23:43:42 +0900

    1.리스트 랜더링 추가 2.이벤트 핸들러 추가

--- rendering/list.js
+++ rendering/list.js
@@ -4,7 +4,7 @@ Vue.component ('myform', {
 });
 
 new Vue({
-  el:'#app1',
+  el:'#app8',
   data: {
     newTodoText :'',
     newTodoId : 4,
@@ -20,3 +20,45 @@ new Vue({
     }
   }
 });
+
+
+Vue.component('todo-item', {
+  template: '\
+    <li>\
+      {{ title }}\
+      <button v-on:click="$emit(\'remove\')">Remove</button>\
+    </li>\
+  ',
+  props: ['title']
+})
+
+new Vue({
+  el: '#app8',
+  data: {
+    newTodoText: '',
+    todos: [
+      {
+        id: 1,
+        title: 'Do the dishes',
+      },
+      {
+        id: 2,
+        title: 'Take out the trash',
+      },
+      {
+        id: 3,
+        title: 'Mow the lawn'
+      }
+    ],
+    nextTodoId: 4
+  },
+  methods: {
+    addNewTodo: function () {
+      this.todos.push({
+        id: this.nextTodoId++,
+        title: this.newTodoText
+      })
+      this.newTodoText = ''
+    }
+  }
+})
