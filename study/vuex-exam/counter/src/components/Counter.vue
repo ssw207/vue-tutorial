9433f54f2709b87d2588176e375380514c95d2b7
Woo KIM <ssw890720@gmail.com>
2020-01-25 23:29:54 +0900

    1.프로젝트 구조 변경 2.todo-app 초기 커밋

---
+++ study/vuex-exam/counter/src/components/Counter.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    {{ this.$store.state.count }}
+    <button @click="inc">+</button>
+    <button @click="dec">-</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    inc() {
+      this.$store.commit("inc", { count: 1 });
+    },
+    dec() {
+      this.$store.commit("dec", { count: 1 });
+    }
+  }
+};
+</script>
+
+<style></style>
