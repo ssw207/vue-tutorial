d5bdd83968fd1bd2c94b30e543ceb76717175f1f
swsong <ssw890720@gmail.com>
2022-12-29 00:05:55 +0900

    feat: 클릭한 평균시간을 측정하는 기능 추가

--- study/zerocho/response-check/ResponseCheck.vue
+++ study/zerocho/response-check/ResponseCheck.vue
@@ -3,7 +3,7 @@
     <!--v-bind 설정시 date의 state 데이터를 값으로 쓸수 있다 : 로 축약가능-->
     <div id="screen" :class="state" @click="onClickScreen">{{ message }}</div>
     <div>
-      <div>평균 시간 : {{ time }}</div>
+      <div>평균 시간 : {{result.reduce((a, b) => a + b, 0) / result.length || 0}}ms</div>
       <button @click="onReset">리셋</button>
     </div>
   </div>
@@ -11,19 +11,23 @@
 
 <script>
 
+let startTime = 0;
+let endTime = 0;
+let timeOut = null;
 
 export default {
   data() {
     return {
       message: '눌려주세요',
       state: 'waiting',
-      time: 0,
+      result: [],
     }
   },
   methods: {
     onReset() {
-      this.message = '';
-      this.time = 0;
+      this.message = '눌러주세요';
+      this.result = [];
+      this.state = 'waiting';
     },
     onClickScreen() {
 
@@ -31,14 +35,22 @@ export default {
       // console.log('시작', state);
 
       if (state == 'waiting') {
-        this.message = '준비';
+        this.message = '초록색이 되면 클릭하세요';
         this.state = 'ready';
+        timeOut = setTimeout(() => {
+          this.state = 'now';
+          this.message = '지금 클릭';
+          startTime = new Date();
+        }, Math.floor(Math.random() * 1000) + 2000); // 2-3초
       } else if (state == 'ready') {
-        this.message = '지금!';
-        this.state = 'now';
+        clearTimeout(timeOut);
+        this.message = '너무 성급하시네요 초록색이 된수에 변경해쥇요';
+        this.state = 'ready';
       } else if (state == 'now') {
+        endTime = new Date();
         this.message = '준비';
         this.state = 'waiting';
+        this.result.push(endTime - startTime);
       }
     }
   },
